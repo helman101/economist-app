c19ea78691563e6ae7fe1082c64fb465e04141d4
helman101 <helmanandresortegon@gmail.com>
2021-08-30 15:43:09 +0200

    create root reducer

---
+++ src/reducers/rootReducer.js
@@ -0,0 +1,11 @@
+import userReducer from './userReducer'
+import stateReducer from './stateReducer'
+import columnReducer from './columnReducer';
+
+const rootReducer = combineReducers({
+  user: userReducer,
+  column: columnReducer,
+  state: stateReducer,
+});
+
+export default rootReducer
