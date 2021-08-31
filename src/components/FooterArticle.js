9e51eaac522285bb2eed7e48f048d92e05c675b4
helman101 <helmanandresortegon@gmail.com>
2021-08-31 12:22:02 +0200

    create footer articles component

---
+++ src/components/FooterArticle.js
@@ -0,0 +1,26 @@
+import React from 'react';
+import PropTypes from 'prop-types';
+
+const FooterArticle = (props) => {
+  const { article } = props
+
+  return (
+    <div>
+      { article.img_url && <div><img src={article.img_url} atl={article.title} /></div> } 
+      <h3>
+        <a href={article.url}>
+          {article.title}
+        </a>
+      </h3>
+      <p>
+        {article.description}
+      </p>
+    </div>
+  );
+};
+
+FooterArticle.PropTypes = {
+  article: PropTypes.shape({ title: PropTypes.string.isRequired, url: PropTypes.string.isRequired, description: PropTypes.string.isRequired, img_url: PropTypes.string.isRequired }).isRequired
+}
+
+export default FooterArticle;
