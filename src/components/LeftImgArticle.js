f53a44f52a87bd6559addc8e60e1565f65e5a374
helman101 <helmanandresortegon@gmail.com>
2021-09-02 16:19:27 +0200

    add element to display priority

--- src/components/LeftImgArticle.js
+++ src/components/LeftImgArticle.js
@@ -3,22 +3,33 @@ import PropTypes from 'prop-types';
 
 const LeftImgArticle = (props) => {
   const { article } = props;
+  const {
+    description,
+    title,
+    url,
+    priority,
+  } = article;
 
   return (
     <div className="small-article p-3">
-      <a className="d-flex justify-content-between" href={article.url}>
+      <a className="d-flex justify-content-between align-items-center" href={url}>
+        { (!description && !article.img_url) && (
+          <div className="priority text-danger me-4">
+            {priority}
+          </div>
+        )}
         <div className={`${article.img_url ? 'w-70' : 'w-100'}`}>
           <h3>
-            {article.title}
+            {title}
           </h3>
           <p>
-            {article.description}
+            {description}
           </p>
         </div>
 
         { article.img_url && (
           <div className="w-33 h-100 position-relative">
-            <img className="w-100" src={article.img_url} alt={article.title} />
+            <img className="w-100" src={article.img_url} alt={title} />
           </div>
         )}
       </a>
@@ -32,6 +43,7 @@ LeftImgArticle.propTypes = {
     url: PropTypes.string.isRequired,
     description: PropTypes.string,
     img_url: PropTypes.string,
+    priority: PropTypes.number,
   }).isRequired,
 };
 
