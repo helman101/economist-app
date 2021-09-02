68e00f4150461c341780a48b8e7bb1ed225d337a
helman101 <helmanandresortegon@gmail.com>
2021-09-02 13:02:56 +0200

    prevent unlogged users to see the articles

--- src/containers/ColumnContainer.js
+++ src/containers/ColumnContainer.js
@@ -1,11 +1,28 @@
-import React from 'react';
+import React, { useEffect } from 'react';
+import { toast } from 'react-toastify';
 import { connect } from 'react-redux';
 import PropTypes from 'prop-types';
 import TopColumns from './TopColumns';
 import FooterColumns from './FooterColumns';
 
 const ColumnContainer = (props) => {
-  const { columns } = props;
+  const { columns, user } = props;
+
+  useEffect(() => {
+    if (Object.keys(user).length === 0) {
+      props.history.push('/');
+      toast.error('Access denied, please Log in', {
+        position: 'top-right',
+        autoClose: 5000,
+        hideProgressBar: false,
+        closeOnClick: true,
+        pauseOnHover: true,
+        draggable: true,
+        progress: undefined,
+        theme: 'colored',
+      });
+    }
+  });
 
   return (
     <div className="container">
@@ -17,10 +34,13 @@ const ColumnContainer = (props) => {
 
 ColumnContainer.propTypes = {
   columns: PropTypes.instanceOf(Array).isRequired,
+  user: PropTypes.instanceOf(Object).isRequired,
+  history: PropTypes.shape({ push: PropTypes.func.isRequired }).isRequired,
 };
 
 const mapStateToProps = (state) => ({
   columns: state.columns,
+  user: state.user,
 });
 
 export default connect(mapStateToProps)(ColumnContainer);
