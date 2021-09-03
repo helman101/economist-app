a54dc8389eb7cabdce3b1961d097b0aa031c2769
helman101 <helmanandresortegon@gmail.com>
2021-09-03 12:13:53 +0200

    add defaultprop for w

--- src/containers/BigArticlesCont.js
+++ src/containers/BigArticlesCont.js
@@ -8,15 +8,19 @@ const BigArticlesCont = (props) => {
   return (
     <div className={`${w ? 'w-100' : 'w-50'} px-3 py-2 border-top border-dark`}>
       { articles.map((article) => (
-        <BigArticle w={w} key={article.id} article={article} />
+        <BigArticle w key={article.id} article={article} />
       )) }
     </div>
   );
 };
 
+BigArticlesCont.defaultProps = {
+  w: false,
+};
+
 BigArticlesCont.propTypes = {
   articles: PropTypes.instanceOf(Array).isRequired,
-  w: PropTypes.bool.isRequired,
+  w: PropTypes.bool,
 };
 
 export default BigArticlesCont;
